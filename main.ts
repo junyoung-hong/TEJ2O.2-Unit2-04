cadcee1eaa38f3a22763b347a1d894d7fd3d4f47
junyoung-hong <junyoung.hong@stu.ocsb.ca>
2026-02-20 13:29:35 -0500

    not done yet

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2026 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Junyoung
+ * Created on: Feb 2026
+ * This program shows a temperature when I press A button
 */
 
-basic.showString('Hello, World!')
+
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() 
+ {input.temperature()
+  basic.showString('The temperature is :21')})
