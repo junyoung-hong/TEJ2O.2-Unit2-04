4c76fa1dc8b4f9a8e672123a8299e073a2964584
junyoung-hong <junyoung.hong@stu.ocsb.ca>
2026-02-27 12:54:46 -0500

    TEMPERATURE SHOWING CODE

--- main.ts
+++ main.ts
@@ -5,11 +5,15 @@
  * This program shows a temperature when I press A button
 */
 
-
+// our variable for a random number
+let temperatureValueCelsius: number
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-input.onButtonPressed(Button.A, function() 
- {input.temperature()
-  basic.showString('The temperature is :21')})
+input.onButtonPressed(Button.A, function () {
+    //get temperature value 
+    temperatureValueCelsius = input.temperature()
+    //output answer 
+    basic.clearScreen()
+    basic.showString('The temperature is :' + temperatureValueCelsius.toString() + 'C' )})
